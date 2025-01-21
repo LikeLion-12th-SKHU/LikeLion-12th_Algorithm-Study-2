63e4577d2f880be99a3009d1b2e146d9c0adc836
Junyeong-An <2117ab@gmail.com>
2025-01-21 23:26:30 +0900

    [BOJ] 11659 구간 합 구하기 4 / 실버 3 / 25분

---
+++ junyeong/src/boj_11659/Main.java
@@ -0,0 +1,36 @@
+package boj_11659;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[N + 1];
+        int[] sum = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+            sum[i] = sum[i - 1] + numbers[i];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int q = 0; q < M; q++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            int realSum = sum[j] - sum[i - 1];
+            sb.append(realSum).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
+
